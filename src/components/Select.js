311368d4ed912ce41f91ac4c0e267bb4ddc6ce53
Matheus Silva <matheussgsilva@hotmail.com>
2023-08-25 20:14:15 -0300

    feat: Create Input and Select styled component

---
+++ src/components/Select.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export default styled.select`
+    width: 100%;
+    background: #FFF;
+    border: 2px solid #FFF;
+    box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.15);
+    height: 52px;
+    border-radius: 4px;
+    border: none;
+    outline: none;
+    padding: 0 16px;
+    font-size: 16px;
+    transition: border-color 0.2 ease-in;
+
+    &:focus {
+        border-color: ${({ theme }) => theme.colors.primary.main};
+    }
+`;
