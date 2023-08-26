8246186d895ab26d9af368b243ccab716271117f
Matheus Silva <matheussgsilva@hotmail.com>
2023-08-25 21:02:56 -0300

    feat: Create Button styled component

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -2,6 +2,7 @@ import PageHeader from '../../components/PageHeader';
 
 import Input from '../../components/Input';
 import Select from '../../components/Select';
+import Button from '../../components/Button';
 
 export default function NewContact() {
   return (
@@ -12,6 +13,10 @@ export default function NewContact() {
       <Select>
         <option value="123">Instagram</option>
       </Select>
+
+      <Button type="button">Criar contato</Button>
+
+      <Button type="button" disabled>Criar contato</Button>
     </>
   );
 }
