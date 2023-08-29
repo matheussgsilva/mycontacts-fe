dcae4ab608a67ae956741ec06626608ecb5af742
Matheus Silva <matheussgsilva@hotmail.com>
2023-08-28 21:27:19 -0300

    refactor: Change Modal render to a Portal

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,11 +1,12 @@
 import Proptypes from 'prop-types';
+import ReactDOM from 'react-dom';
 
 import { Container, Overlay, Footer } from './styles';
 
 import Button from '../Button';
 
 export default function Modal({ danger }) {
-  return (
+  return ReactDOM.createPortal(
     <Overlay>
       <Container danger={danger}>
         <h1>Título do Modal</h1>
@@ -21,7 +22,8 @@ export default function Modal({ danger }) {
           </Button>
         </Footer>
       </Container>
-    </Overlay>
+    </Overlay>,
+    document.getElementById('modal-root'),
   );
 }
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,7 +4,6 @@ import {
   Container, Header, ListContainer, Card, InputSearchContainer,
 } from './styles';
 
-import Modal from '../../components/Modal';
 import arrow from '../../assets/images/icons/arrow.svg';
 import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
@@ -12,8 +11,6 @@ import trash from '../../assets/images/icons/trash.svg';
 export default function Home() {
   return (
     <Container>
-
-      <Modal danger />
 
       <InputSearchContainer>
         <input type="text" placeholder="Pesquisar Contato" />
