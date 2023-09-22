f08bf908d7fd7c30ea9d7e7a992d514f5423e966
Matheus Silva <matheussgsilva@hotmail.com>
2023-09-22 20:33:29 -0300

    refactor: Turn function loadContacts in try-catch

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -22,19 +22,22 @@ export default function Home() {
   )), [contacts, searchTerm]);
 
   useEffect(() => {
-    setIsLoading(true);
+    async function loadContacts() {
+      try {
+        setIsLoading(true);
 
-    fetch(`http://localhost:3001/contacts?orderBy=${orderBy}`)
-      .then(async (response) => {
+        const response = await fetch(`http://localhost:3001/contacts?orderBy=${orderBy}`);
+
         const json = await response.json();
         setContacts(json);
-      })
-      .catch((error) => {
-        console.log(error);
-      })
-      .finally(() => {
+      } catch (error) {
+        console.log('error', error);
+      } finally {
         setIsLoading(false);
-      });
+      }
+    }
+
+    loadContacts();
   }, [orderBy]);
 
   function handleToggleOrderBy() {
