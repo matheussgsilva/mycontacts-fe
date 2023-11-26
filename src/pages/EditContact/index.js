1334320ed88e4f64007089c76df25a0c410908cf
Matheus Silva <matheussgsilva@hotmail.com>
2023-11-26 19:01:40 -0300

    feat: Create function to get contact in Edit Page

--- src/pages/EditContact/index.js
+++ src/pages/EditContact/index.js
@@ -1,11 +1,56 @@
+import { useEffect, useState } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+
 import ContactForm from '../../components/ContactForm';
+import Loader from '../../components/Loader';
 import PageHeader from '../../components/PageHeader';
 
+import ContactsService from '../../services/ContactsService';
+import toast from '../../utils/toast';
+
 export default function EditContact() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const { id } = useParams();
+  const history = useHistory();
+
+  useEffect(() => {
+    async function loadContact() {
+      try {
+        const contactData = await ContactsService.getContactById(
+          id,
+        );
+
+        console.log({ contactData });
+        setIsLoading(false);
+      } catch {
+        history.push('/');
+        toast({
+          type: 'danger',
+          text: 'Contato não encontrado!',
+        });
+      }
+    }
+
+    loadContact();
+  }, [id, history]);
+
+  const handleSubmit = () => {
+    //
+  };
+
   return (
     <>
-      <PageHeader title="Editar Matheus Silva" />
-      <ContactForm buttonLabel="Salvar Alterações" />
+      <Loader isLoading={isLoading} />
+
+      <PageHeader
+        title="Editar Matheus Silva"
+      />
+
+      <ContactForm
+        buttonLabel="Salvar Alterações"
+        onSubmit={handleSubmit}
+      />
     </>
   );
 }
